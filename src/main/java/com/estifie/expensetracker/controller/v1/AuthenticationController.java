29f5c905d949d7a7cbbd8118495b96c53bdc5cb0
LntAxe <ertusari0@gmail.com>
2024-10-06 21:56:49 +0300

    feat: Add AuthenticationController for user registration and login

---
+++ src/main/java/com/estifie/expensetracker/controller/v1/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.estifie.expensetracker.controller.v1;
+
+import com.estifie.expensetracker.dto.auth.UserLoginDTO;
+import com.estifie.expensetracker.dto.auth.UserRegisterDTO;
+import com.estifie.expensetracker.response.ApiResponse;
+import com.estifie.expensetracker.response.auth.UserLoginResponse;
+import com.estifie.expensetracker.service.AuthenticationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<Void>> register(@Valid @RequestBody UserRegisterDTO userRegisterDTO) {
+        authenticationService.register(userRegisterDTO);
+        return ResponseEntity.ok(ApiResponse.success());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<UserLoginResponse>> login(@Valid @RequestBody UserLoginDTO userLoginDTO) {
+        String token = authenticationService.login(userLoginDTO);
+
+        return ResponseEntity.ok(ApiResponse.<UserLoginResponse>success().data(new UserLoginResponse(token)));
+    }
+}
