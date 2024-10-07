eab3e117ea13bd14bf2d00c5e2ca671fe8b6d0cf
LntAxe <ertusari0@gmail.com>
2024-10-07 11:57:30 +0300

    refactor: Update UserService interface to include additional methods for managing user status and permissions

--- src/main/java/com/estifie/expensetracker/service/UserService.java
+++ src/main/java/com/estifie/expensetracker/service/UserService.java
@@ -2,11 +2,25 @@ package com.estifie.expensetracker.service;
 
 import com.estifie.expensetracker.model.User;
 
+import java.util.List;
 import java.util.Set;
 
 public interface UserService {
     User findByUsername(String username);
-
+    List<User> findAll();
+    List<User> findAllActive();
+    List<User> findAllDeleted();
+    List<User> findAllDeactivated();
+    long countAll();
+    long countActive();
+    long countDeleted();
+    long countDeactivated();
+    void deactivate(String username);
+    void activate(String username);
+    void delete(String username);
+    void restore(String username);
+    boolean isUserDeleted(String username);
+    boolean isUserActive(String username);
     void grantPermission(String username, String permissionName);
     void revokePermission(String username, String permissionName);
     Set<String> getPermissions(String username);
