33a1fea438372dbe0002cdc44d55c65b4464c48d
LntAxe <ertusari0@gmail.com>
2024-10-09 13:21:43 +0300

    refactor: Change currency conversion functionality in CurrencyConversionServiceImpl

--- src/main/java/com/estifie/expensetracker/service/CurrencyConversionServiceImpl.java
+++ src/main/java/com/estifie/expensetracker/service/CurrencyConversionServiceImpl.java
@@ -1,38 +1,72 @@
 package com.estifie.expensetracker.service;
 
 import com.estifie.expensetracker.exception.currency.CurrencyConversionException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import yahoofinance.YahooFinance;
-import yahoofinance.quotes.fx.FxQuote;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 @Service
 public class CurrencyConversionServiceImpl implements CurrencyConversionService {
     private final CurrencyCacheService currencyCacheService;
+    private final RestTemplate restTemplate;
+    private final String currencyConversionApiUrl;
+    private final String currencyConversionApiKey;
 
-    public CurrencyConversionServiceImpl(CurrencyCacheService currencyCacheService) {
+    public CurrencyConversionServiceImpl(CurrencyCacheService currencyCacheService, RestTemplate restTemplate, @Value("${currency-conversion.api.url}") String currencyConversionApiUrl, @Value("${currency-conversion.api.key}") String currencyConversionApiKey) {
         this.currencyCacheService = currencyCacheService;
+        this.restTemplate = restTemplate;
+        this.currencyConversionApiUrl = currencyConversionApiUrl;
+        this.currencyConversionApiKey = currencyConversionApiKey;
     }
 
     public BigDecimal convertCurrency(String fromCurrency, String toCurrency, BigDecimal amount) {
         String currencyPair = fromCurrency + toCurrency;
+
         BigDecimal exchangeRate = currencyCacheService.get(currencyPair)
                 .orElseGet(() -> getExchangeRateExternal(fromCurrency, toCurrency));
+
         return amount.multiply(exchangeRate);
     }
 
     private BigDecimal getExchangeRateExternal(String fromCurrency, String toCurrency) {
         String currencyPair = fromCurrency + toCurrency;
+
         try {
-            FxQuote quote = YahooFinance.getFx(currencyPair);
-            if (quote == null || quote.getPrice() == null) {
+
+
+            String url = UriComponentsBuilder.fromHttpUrl(currencyConversionApiUrl)
+                    .queryParam("apiKey", currencyConversionApiKey)
+                    .queryParam("currencies", toCurrency)
+                    .queryParam("base", fromCurrency)
+                    .toUriString();
+
+            System.out.println("Constructed URL: " + url);
+
+            ParameterizedTypeReference<Map<String, Map<String, BigDecimal>>> responseType = new ParameterizedTypeReference<>() {
+            };
+
+            Map<String, Map<String, BigDecimal>> response = restTemplate.exchange(url, HttpMethod.GET, null, responseType)
+                    .getBody();
+
+            if (response == null || response.get("data") == null) {
                 throw new CurrencyConversionException("No exchange rate available for " + currencyPair);
             }
 
-            currencyCacheService.put(currencyPair, quote.getPrice());
+            BigDecimal exchangeRate = response.get("data")
+                    .get(toCurrency);
+            if (exchangeRate == null) {
+                throw new CurrencyConversionException("No exchange rate available for " + currencyPair);
+            }
 
-            return quote.getPrice();
+            currencyCacheService.put(currencyPair, exchangeRate);
+
+            return exchangeRate;
         } catch (Exception e) {
             throw new CurrencyConversionException("Error while fetching exchange rate");
         }
