13d1e6746636b242c0b2e4176bd467005b238b56
LntAxe <ertusari0@gmail.com>
2024-10-09 20:57:13 +0300

    refactor: Remove unnecessary whitespace in UserLoginResponse constructor

--- src/main/java/com/estifie/expensetracker/response/auth/UserLoginResponse.java
+++ src/main/java/com/estifie/expensetracker/response/auth/UserLoginResponse.java
@@ -2,7 +2,7 @@ package com.estifie.expensetracker.response.auth;
 
 public class UserLoginResponse {
     public String token;
-
+    
     public UserLoginResponse(String token) {
         this.token = token;
     }
