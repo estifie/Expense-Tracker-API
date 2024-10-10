aa1b8520b76ec3729d54bbec8a95366a52646444
LntAxe <ertusari0@gmail.com>
2024-10-11 00:48:04 +0300

    feat: Add SubscriptionCreateDTO class

---
+++ src/main/java/com/estifie/expensetracker/dto/subscription/SubscriptionCreateDTO.java
@@ -0,0 +1,60 @@
+package com.estifie.expensetracker.dto.subscription;
+
+import com.estifie.expensetracker.enums.SubscriptionType;
+import com.estifie.expensetracker.model.Subscription;
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class SubscriptionCreateDTO {
+    @NotBlank
+    private final String name;
+    @NotBlank
+    private final LocalDate startDate;
+    @NotBlank
+    private final SubscriptionType type;
+    @NotBlank
+    private BigDecimal amount;
+    private String currencyCode;
+
+    public SubscriptionCreateDTO(String name, BigDecimal amount, LocalDate startDate, SubscriptionType type, String currencyCode) {
+        this.name = name;
+        this.amount = amount;
+        this.startDate = startDate;
+        this.type = type;
+        this.currencyCode = currencyCode;
+    }
+
+    public @NotBlank String getName() {
+        return name;
+    }
+
+    public @NotBlank BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(@NotBlank BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public @NotBlank LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public @NotBlank SubscriptionType getType() {
+        return type;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public Subscription toSubscription() {
+        return new Subscription(name, amount, startDate, type, currencyCode);
+    }
+}
