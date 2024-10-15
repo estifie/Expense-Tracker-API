7671fa62a54a0791d93537f893448b74a957926a
LntAxe <ertusari0@gmail.com>
2024-10-15 21:05:25 +0300

    refactor: Update TagRepository interface

--- src/main/java/com/estifie/expensetracker/repository/TagRepository.java
+++ src/main/java/com/estifie/expensetracker/repository/TagRepository.java
@@ -1,4 +1,15 @@
 package com.estifie.expensetracker.repository;
 
-public interface TagRepository {
+import com.estifie.expensetracker.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, String> {
+    Optional<Tag> findByName(String name);
+
+    Optional<Tag> findByIdAndDeletedAtIsNull(String id);
+
+    List<Tag> findAllByDeletedAtIsNull();
 }
