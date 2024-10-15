1cbac97bf6b4f708f90fd353bf07c675668c2941
LntAxe <ertusari0@gmail.com>
2024-10-15 21:05:44 +0300

    refactor: Enable method-level security configuration

--- src/main/java/com/estifie/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/com/estifie/expensetracker/ExpenseTrackerApplication.java
@@ -2,8 +2,10 @@ package com.estifie.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class ExpenseTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExpenseTrackerApplication.class, args);
