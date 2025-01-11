9a5b9c671f70565c9a924d0ebc4fd09a056fe98b
LntAxe <ertusari0@gmail.com>
2025-01-12 00:13:52 +0300

    feat: Add swagger support

---
+++ src/main/java/com/estifie/expensetracker/config/OpenAPIConfig.java
@@ -0,0 +1,59 @@
+package com.estifie.expensetracker.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Expense Tracker API",
+                version = "1.0",
+                description = "This API provides endpoints for managing personal expenses, categories, and subscriptions. " +
+                        "It includes features for expense tracking, budget management, and subscription monitoring.",
+                contact = @Contact(
+                        url = "https://github.com/estifie"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "Local Environment",
+                        url = "http://localhost:8080/api"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/v1/**")
+                .build();
+    }
+} 
